24d45f5a667832d4e0876b614df6ee4a78056d56
Johannes Hoppe <johannes.hoppe@haushoppe-its.de>
2019-01-17 13:56:05 +0100

    TODO: buch zum array hinzufügen

---
+++ book-rating/src/app/books/create-book/create-book.component.ts
@@ -0,0 +1,42 @@
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'br-create-book',
+  templateUrl: './create-book.component.html',
+  styleUrls: ['./create-book.component.scss']
+})
+export class CreateBookComponent {
+
+  bookForm = new FormGroup({
+    isbn: new FormControl('', [
+      Validators.required,
+      Validators.minLength(3)
+    ]),
+    title: new FormControl('', Validators.required),
+    description: new FormControl('')
+  });
+
+  isInvalid(name: string) {
+    const control = this.bookForm.get(name);
+    return control.invalid && control.dirty;
+  }
+
+  hasError(name: string, errorCode: string) {
+    const control = this.bookForm.get(name);
+    return control.hasError(errorCode) && control.dirty;
+  }
+
+  submitFirm() {
+    const newBook = {
+      ...this.bookForm.value,
+      rating: 1
+    };
+
+    // TODO
+    // 1. Event emitten: "created" mit Payload: Buch
+    // 2. Buch im Dashboard empfangen, zum Array hinzufügen
+
+    this.bookForm.reset();
+  }
+}
