9ce8948dba0b0f633c12e6f3069d20778bc4b6a9
Johannes Hoppe <johannes.hoppe@haushoppe-its.de>
2019-01-18 13:47:16 +0100

    share makes observables hot

--- book-rating/src/app/books/book-details/book-details.component.ts
+++ book-rating/src/app/books/book-details/book-details.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { BookStoreService } from '../shared/book-store.service';
 import { Book } from '../shared/book';
-import { map, switchMap, filter, reduce, scan, tap, catchError, mergeMap } from 'rxjs/operators';
+import { map, switchMap, filter, reduce, scan, tap, catchError, mergeMap, share } from 'rxjs/operators';
 import { HttpErrorResponse } from '@angular/common/http';
 import { of, timer, interval, Observable, never, EMPTY } from 'rxjs';
 
@@ -11,28 +11,21 @@ import { of, timer, interval, Observable, never, EMPTY } from 'rxjs';
   templateUrl: './book-details.component.html',
   styleUrls: ['./book-details.component.scss']
 })
-export class BookDetailsComponent implements OnInit, OnDestroy {
+export class BookDetailsComponent implements OnInit {
 
-  book: Book;
+  book$: Observable<Book>;
 
   constructor(private route: ActivatedRoute, private bookService: BookStoreService) {
   }
 
-  ngOnDestroy() {
-    // hier subscriptions unsubscriben!!!
-  }
-
   ngOnInit() {
 
-    this.route.paramMap.pipe(
+    this.book$ = this.route.paramMap.pipe(
       map(params => params.get('isbn')),
       switchMap(isbn => this.bookService.getBook(isbn)
-        .pipe(
-          catchError(() => EMPTY)
-        ))
-      // catchError - HIER NICHT
-    ).subscribe(
-      book => this.book = book
+        .pipe(catchError(() => EMPTY))
+      ),
+      share()
     );
   }
 }
