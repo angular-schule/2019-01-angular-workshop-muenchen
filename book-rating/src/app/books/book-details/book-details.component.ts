ce52db966065648a9a5280ec0af3c0d1e5152901
Johannes Hoppe <johannes.hoppe@haushoppe-its.de>
2019-01-18 12:41:41 +0100

    rxjs building blocks

--- book-rating/src/app/books/book-details/book-details.component.ts
+++ book-rating/src/app/books/book-details/book-details.component.ts
@@ -1,21 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { BookStoreService } from '../shared/book-store.service';
 import { Book } from '../shared/book';
-import { map, switchMap } from 'rxjs/operators';
+import { map, switchMap, filter, reduce, scan, tap, catchError } from 'rxjs/operators';
 import { HttpErrorResponse } from '@angular/common/http';
-import { of, timer, interval } from 'rxjs';
+import { of, timer, interval, Observable } from 'rxjs';
 
 @Component({
   selector: 'br-book-details',
   templateUrl: './book-details.component.html',
   styleUrls: ['./book-details.component.scss']
 })
-export class BookDetailsComponent implements OnInit {
+export class BookDetailsComponent implements OnInit, OnDestroy {
 
   book: Book;
 
   constructor(private route: ActivatedRoute, private bookService: BookStoreService) {
+  }
+
+  ngOnDestroy() {
+    // hier subscriptions unsubscriben!!!
   }
 
   ngOnInit() {
@@ -25,14 +29,34 @@ export class BookDetailsComponent implements OnInit {
 
     // interval(2000).subscribe(e => console.log(e));
 
-    // creation function
-    of('A', 'B', 'C').subscribe(
-      d => console.log(d),
-      () => {},
-      () => console.log('COMPLETED!')
-    );
+    const observer = {
+      next: d => console.log(d),
+      error: e => console.error(e),
+      complete: () => console.log('COMPLETED!')
+    };
 
+    const myObservable$ = new Observable<number>(observer2 => {
 
+      observer2.next(1);
+      observer2.next(2);
+
+      setTimeout(() => observer2.next(3), 1000);
+      setTimeout(() => observer2.next(4), 2000);
+      setTimeout(() => observer2.error('Mist!'), 3000);
+      // setTimeout(() => observer2.complete(), 3000);
+    });
+
+    const subscription = myObservable$
+      .pipe(
+        map(d => d * 10),
+        filter(d => d !== 20),
+        tap(d => console.log('TAP', d)),
+        scan((acc, value) => acc + value),
+        catchError(e => of('😀'))
+      )
+      .subscribe(observer);
+
+    // setTimeout(() => subscription.unsubscribe(), 1000);
 
 
 
@@ -48,7 +72,8 @@ export class BookDetailsComponent implements OnInit {
 
     this.route.paramMap.pipe(
       map(params => params.get('isbn')),
-      switchMap(isbn => this.bookService.getBook(isbn))
+      switchMap(isbn => this.bookService.getBook(isbn)),
+      // catchError
     ).subscribe(
       book => this.book = book,
       (e: HttpErrorResponse) => this.book = {
